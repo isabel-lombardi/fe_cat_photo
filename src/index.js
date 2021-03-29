16eaaea0e52ea8a8cd85f16f1c125d4cc97b0886
Manuel Alonge <manuel.alonge@gmail.com>
2021-03-29 23:29:24 +0200

    test-commit

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const password = document.getElementById('password');
 // Event Listener for Login function
 loginForm.addEventListener('submit', (e) => {
   // e.preventDefault();
-  fetch('https://cat-photo.herokuapp.com/login/', { // fetch not working
+  fetch('https://cat-photo.herokuapp.com/login/', {
     method: 'post',  
     headers: {
       'Content-Type': 'application/json',
